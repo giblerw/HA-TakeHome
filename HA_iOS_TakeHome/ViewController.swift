1af6cc6931ff182b36651c6bf65c49f4d0609bf7
GiblerW <westongibler@gmail.com>
2018-10-17 18:05:28 -0600

    Initial Commit

---
+++ HA_iOS_TakeHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HA_iOS_TakeHome
+//
+//  Created by Weston Gibler on 10/17/18.
+//  Copyright © 2018 Weston Gibler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HA_iOS_TakeHomeTests/HA_iOS_TakeHomeTests.swift
@@ -0,0 +1,34 @@
+//
+//  HA_iOS_TakeHomeTests.swift
+//  HA_iOS_TakeHomeTests
+//
+//  Created by Weston Gibler on 10/17/18.
+//  Copyright © 2018 Weston Gibler. All rights reserved.
+//
+
+import XCTest
+@testable import HA_iOS_TakeHome
+
+class HA_iOS_TakeHomeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
