fe2ddfb00737980972ceb4d9e08cf2273f48ca8c
GiblerW <westongibler@gmail.com>
2018-10-22 17:44:15 -0600

    nice and clean

---
+++ HA_iOS_TakeHome/Models/ProItem.swift
@@ -0,0 +1,9 @@
+//
+//  ProItem.swift
+//  HA_iOS_TakeHome
+//
+//  Created by Weston Gibler on 10/22/18.
+//  Copyright © 2018 Weston Gibler. All rights reserved.
+//
+
+import Foundation
--- HA_iOS_TakeHome/Views/ProsCell.swift
+++ HA_iOS_TakeHome/Views/ProsCell.swift
@@ -16,19 +16,23 @@ class ProsCell: UITableViewCell {
          let ratingTextString = "Rating: \(proList.compositeRating) | \(proList.ratingCount) rating(s)"
         guard let ratingRank = Double(proList.compositeRating) else { return }
        
-            if ratingRank >= 4.0 {
-                ratingLabel.text = ratingTextString
-                ratingLabel.textColor = #colorLiteral(red: 0.09945433747, green: 0.7647058964, blue: 0.2298767669, alpha: 1)
-            } else if ratingRank >= 3.0 && ratingRank < 4.0{
-                ratingLabel.text = ratingTextString
-                ratingLabel.textColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
-            } else if ratingRank < 3.0 && ratingRank > 0 {
-                ratingLabel.text = ratingTextString
-                ratingLabel.textColor = #colorLiteral(red: 0.7647058964, green: 0.2383460997, blue: 0.2243335623, alpha: 1)
-            } else {
-                ratingLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-                ratingLabel.text = "References Available"
-            }
+        switch ratingRank {
+        case 0:
+            ratingLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            ratingLabel.text = "References Available"
+        case 1..<3.0:
+            ratingLabel.text = ratingTextString
+            ratingLabel.textColor = #colorLiteral(red: 0.7647058964, green: 0.2383460997, blue: 0.2243335623, alpha: 1)
+        case 3..<4:
+            ratingLabel.text = ratingTextString
+            ratingLabel.textColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        case 4...:
+            ratingLabel.text = ratingTextString
+            ratingLabel.textColor = #colorLiteral(red: 0.09945433747, green: 0.7647058964, blue: 0.2298767669, alpha: 1)
+        default:
+            ratingLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            ratingLabel.text = "References Available"
+        }
         prosNameLabel.text = proList.companyName
     }
 }
